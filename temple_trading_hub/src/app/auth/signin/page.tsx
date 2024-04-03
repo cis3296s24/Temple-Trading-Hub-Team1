d345c5f5499c8f85ae6402dbb6f30ce7a7ff5150
ishmam02 <143619983+tuo47952@users.noreply.github.com>
2024-04-03 12:25:17 -0400

    Api created

--- temple_trading_hub/src/app/auth/signin/page.tsx
+++ temple_trading_hub/src/app/auth/signin/page.tsx
@@ -1,9 +1,25 @@
-const signIn = () =>{
-    return(
-        <div>
-            SignIn
-        </div>
-    );
-}
+import { Container, Stack, TextField, Typography } from '@mui/material';
+import Link from 'next/link';
+
+const signIn = () => {
+  return (
+    <div>
+      <Container maxWidth='sm'>
+        <Typography variant='h5' component='h5' align='center' margin={2}>
+          Sign In
+        </Typography>
+        New User?<Link href={'/auth/signup'}>Sign Up</Link>
+        <Stack spacing={2}>
+          <TextField
+            label={'margin="dense"'}
+            fullWidth
+            label='Email'
+            id='email'
+          />
+        </Stack>
+      </Container>
+    </div>
+  );
+};
 
 export default signIn;
--- temple_trading_hub/src/app/auth/signup/page.tsx
+++ temple_trading_hub/src/app/auth/signup/page.tsx
@@ -1,9 +1,21 @@
-const signUp = () =>{
-    return(
-        <div>
-            SignUp
-        </div>
-    );
-}
+import { Container, Stack, TextField, Typography } from '@mui/material';
+import Link from 'next/link';
+
+const signUp = () => {
+  return (
+    <div>
+      <Container maxWidth='sm'>
+        <Typography variant='h5' component='h5' align='center' margin={2}>
+          Sign Up
+        </Typography>
+        Existing User?<Link href={'/auth/signin'}>Sign In</Link>
+        <Stack spacing={2}>
+          <TextField fullWidth label='Email' id='email' />
+          <TextField fullWidth label='Password' id='email' />
+        </Stack>
+      </Container>
+    </div>
+  );
+};
 
 export default signUp;
