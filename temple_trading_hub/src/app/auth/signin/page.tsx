89fdd768595c79bd8744ad8e93d4e2ab431e20a0
Troy Witmer <troywitmer@a4-cf-99-7b-44-87.wireless.temple.edu>
2024-04-04 17:00:53 -0400

    rejecting non temple emails and existing emails

--- temple_trading_hub/src/app/auth/signin/page.tsx
+++ temple_trading_hub/src/app/auth/signin/page.tsx
@@ -1,38 +1,72 @@
-'use client';
+"use client"
 
-import { Button, Container, Stack, TextField, Typography } from '@mui/material';
-import Link from 'next/link';
-import { useState } from 'react';
+import { UserAuth } from "@context/AuthContext";
+import { Button, Container, Stack, TextField, Typography } from "@mui/material";
+import { color } from "framer-motion";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const signIn = () => {
-  const [email, setEmail] = useState('');
+  const[email, setEmail] = useState('');
+  const[password, setPassword] = useState('');
 
   //@ts-ignore
-  const handleSignIn = (e) => {
-    e.preventDefault();
-    console.log(`email: ${email}`);
-  };
-
+  const signIn = () => {
+    const router = useRouter();
+    const { user, signIn } = UserAuth();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleSignUp = async (e: any) => {
+      e.preventDefault();
+      await signIn(email, password);
+    };
+  
+    useEffect(() => {
+      const checkAuthentication = async () => {
+        if (user) {
+          router.push('/');
+        }
+      };
+      checkAuthentication();
+    }, [user]);
+  }
+  
   return (
     <div>
-      <Container maxWidth='sm'>
-        <Typography variant='h5' component='h5' align='center' margin={2}>
+      <Container maxWidth="sm">
+        <Typography
+          variant="h5"
+          component="h5"
+          align="center"
+          margin={2}
+        >
           Sign In
         </Typography>
-        <Stack spacing={2} sx={{ backgroundColor: 'white' }}>
-          <form onSubmit={handleSignIn}>
-            <TextField
-              fullWidth
-              label='Email'
-              id='email'
-              //@ts-ignore
-              onChange={(e) => setEmail(e.target.value)}
-            />
+        
+        <Stack spacing={2} sx={{backgroundColor: 'white'}}>
+          <form onSubmit={signIn}>
 
-            <Button type='submit'>Sign in</Button>
+          <TextField
+            fullWidth
+            label="Email"
+            id="email"
+            //@ts-ignore
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            fullWidth
+            label="Password"
+            id="password"
+            //@ts-ignore
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <Button type="submit">Sign in</Button>
+
           </form>
         </Stack>
-        New User? <Link href={'/auth/signup'}>Sign Up</Link>
+        New User? <Link href={"/auth/signup"}>Sign Up</Link>
       </Container>
     </div>
   );
