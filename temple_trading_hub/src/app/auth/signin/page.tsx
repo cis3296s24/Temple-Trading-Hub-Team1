02ccd5e3e1b0ec41632c76383beeea23512f92a8
Troy Witmer <troywitmer@Troys-MacBook-Pro.local>
2024-04-05 21:36:08 -0400

    added formik and yup for text inputs

--- temple_trading_hub/src/app/auth/signin/page.tsx
+++ temple_trading_hub/src/app/auth/signin/page.tsx
@@ -1,13 +1,20 @@
 "use client"
 
+import "../../Styles/global.css";
 import { UserAuth } from "@context/AuthContext";
-import { Button, Container, Stack, TextField, Typography } from "@mui/material";
-import { color } from "framer-motion";
+import { Button, Grid, TextField, Typography } from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import '../../styles/signin.css';
+import { useFormik } from "formik";
+import * as yup from "yup";
 
-//@ts-ignore
+const signInSchema = yup.object({
+  email: yup.string().required("Provide @temple.edu Email").email().min(13, "email too short").max(60, "too long"),
+  password: yup.string().required("Please enter your password")
+})
+
 const signIn = () => {
 
   const router = useRouter();
@@ -15,8 +22,7 @@ const signIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const handleSignIn = async (e: any) => {
-    e.preventDefault();
-    await signIn(email, password);
+    await signIn(e.email, e.password);
   };
 
   useEffect(() => {
@@ -28,45 +34,75 @@ const signIn = () => {
     checkAuthentication();
   }, [user]);
 
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: ""
+    },
+    validationSchema: signInSchema,
+    onSubmit: (values) => {
+      handleSignIn(values);
+    }
+
+  })
+
+
   return (
-    <div>
-      <Container maxWidth="sm">
-        <Typography
-          variant="h5"
-          component="h5"
-          align="center"
-          margin={2}
-        >
+      <Grid 
+      container 
+      justifyContent="center"
+      alignItems="center"
+      direction="column"
+      style={{minHeight: "50vh"}}
+      >
+
+        <Grid item>
+          <Typography className="pageText" variant="h5" color="primary">
           Sign In
-        </Typography>
-
-        <Stack spacing={2} sx={{ backgroundColor: 'white' }}>
-          <form onSubmit={handleSignIn}>
-
-            <TextField
+          </Typography>
+        </Grid>
+        <Grid className="loginComponent" container direction="column" alignItems="center" justifyContent="center">
+          <form onSubmit={formik.handleSubmit}>
+              <TextField 
               fullWidth
-              label="Email"
               id="email"
-              type='email'
-              //@ts-ignore
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
+              name="email"
+              label="Email"
+              variant="filled"
+              className={"outlinedTextField"}
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email} 
+              style={{marginBottom: "1em"}}/>
+
+              <TextField 
               fullWidth
-              label="Password"
               id="password"
-              type='password'
-              //@ts-ignore
-              onChange={(e) => setPassword(e.target.value)}
-            />
+              name="password"
+              label="Password"
+              type="password"
+              variant="filled"
+              className={"outlinedTextField"}
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password} 
+              style={{marginBottom: "1em"}}/>
 
-            <Button type="submit">Sign in</Button>
-
+              <Button className={"submitButton"} type="submit" variant="contained">Sign In</Button>
           </form>
-        </Stack>
-        New User? <Link href={"/auth/signup"}>Sign Up</Link>
-      </Container>
-    </div>
+        </Grid>
+        <Grid item>
+          <Typography variant="body1">
+          Dont have an account? <Link href={'/auth/signup'}>Sign Up</Link>
+          </Typography>
+        </Grid>
+
+      </Grid>
+
   );
 };
 
