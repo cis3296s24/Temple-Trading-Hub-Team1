59a2f84d9cb61a7e063810692d7f6e8a32233cae
VinceLukban <tup76353@temple.edu>
2024-04-20 15:32:07 -0400

    draft of working sign up page

--- temple_trading_hub/src/app/auth/signup/page ORIG.tsx
+++ temple_trading_hub/src/app/auth/signup/page ORIG.tsx
@@ -1,32 +1,57 @@
 'use client';
-import { Avatar, Box, Button, Container, CssBaseline, Grid, TextField, Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
-import { UserAuth } from '@context/AuthContext';
-import { useRouter } from 'next/navigation';
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Grid,
+  TextField,
+  Typography,
+  Link as Li,
+} from '@mui/material';
+import Link from 'next/link';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { UserAuth } from '@context/AuthContext';
 
-const SignUp = () => {
+const signUp = () => {
   const router = useRouter();
-  const { user, signUp } = UserAuth(); // Assuming signUp is a function provided by UserAuth
-
+  const { user, signUp } = UserAuth();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
   const handleSignUp = async (e: any) => {
     e.preventDefault();
-    await signUp(email, password); // Make sure signUp is accessible here
+    await signUp(email, password);
   };
 
   useEffect(() => {
-    if (user) {
-      router.push('/');
-    }
-  }, [user, router]);
-
+    const checkAuthentication = async () => {
+      if (user) {
+        router.push('/');
+      }
+    };
+    checkAuthentication();
+  }, [user]);
   return (
-    <Container component='main' maxWidth='xs'>
+    <Container
+      component='main'
+      maxWidth='xs'
+      sx={{
+        height: '75vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignContent: 'center',
+      }}>
       <CssBaseline />
-      <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
         <Avatar sx={{ m: 1, bgcolor: 'primary.main', color: 'white' }}>
           <LockOutlinedIcon />
         </Avatar>
@@ -35,6 +60,21 @@ const SignUp = () => {
         </Typography>
         <Box component='form' noValidate onSubmit={handleSignUp} sx={{ mt: 3 }}>
           <Grid container spacing={2}>
+            {/* <Grid item xs={12}>
+              <TextField
+                autoComplete='username'
+                name='userName'
+                required
+                fullWidth
+                id='userName'
+                autoFocus
+                hiddenLabel
+                size='small'
+                variant='outlined'
+                aria-label='Username*'
+                placeholder='Username*'
+              />
+            </Grid> */}
             <Grid item xs={12}>
               <TextField
                 required
@@ -67,13 +107,22 @@ const SignUp = () => {
               />
             </Grid>
           </Grid>
-          <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            sx={{ mt: 3, mb: 2 }}>
             Sign Up
           </Button>
+          <Grid container justifyContent='center'>
+            <Grid item>
+              Already have an account? <Li href='/auth/signin'>Sign in</Li>
+            </Grid>
+          </Grid>
         </Box>
       </Box>
     </Container>
   );
 };
 
-export default SignUp;
+export default signUp;
